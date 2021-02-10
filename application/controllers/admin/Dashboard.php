127586f65e0ce85a6f00b0f9199c5e1bb1d67ff9
Mubashar635 <mubashar635@gmail.com>
2021-02-10 13:25:27 +0500

    Update Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -11,8 +11,8 @@ class Dashboard extends Backend_Controller
 	{
 		parent::__construct();
 		$this->load->database();
-		$this->load->library(array('ion_auth', 'form_validation'));
-		$this->load->helper(array('url', 'language'));
+		// $this->load->library(array('ion_auth', 'form_validation'));
+		// $this->load->helper(array('url', 'language'));
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 		$this->load->model('dashboard_model');
